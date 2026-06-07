d03a3c00aad3b38e777661788d1759ae83119f87
Elthon Jhon Kevin <elthonjhonkevin@gmail.com>
2026-06-07 10:48:14 +0700

    Use renamed period hook in create period form

--- ISO-TIK-FE-v2/src/pages/periode/components/CreatePeriodForm.jsx
+++ ISO-TIK-FE-v2/src/pages/periode/components/CreatePeriodForm.jsx
@@ -5,7 +5,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Loader2 } from 'lucide-react'
-import { useCreateForumPeriod } from '@/hooks/useForumPeriod'
+import { useCreatePeriod } from '@/hooks/usePeriod'
 import { createPeriodSchema } from '../constants'
 
 export default function CreatePeriodForm({ onSuccess }) {
@@ -73,7 +73,7 @@ export default function CreatePeriodForm({ onSuccess }) {
     }
   }, [startDate, periodType, endDate, setValue])
 
-  const createPeriodMutation = useCreateForumPeriod({
+  const createPeriodMutation = useCreatePeriod({
     onSuccess: (data, variables, context) => {
       reset()
       if (onSuccess) onSuccess(data, variables, context)
