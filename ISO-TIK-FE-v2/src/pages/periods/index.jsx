e48481f388cbfc35ce4aabe38f6c2e5f44ebb1f0
Elthon Jhon Kevin <elthonjhonkevin@gmail.com>
2026-06-07 09:57:48 +0700

    refactor frontend remove periods alias

--- ISO-TIK-FE-v2/src/pages/periods/index.jsx
+++
@@ -1,3 +0,0 @@
-// Alias halaman Periode.
-// Implementasi lama masih berada di pages/rooms agar refactor dapat dilakukan bertahap tanpa memutus fitur yang sudah berjalan.
-export { default } from '../rooms'
